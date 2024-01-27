55132433a71b9642a7858420980ffac96f82e7a3
Julian Bechthold <julian.bechthold@lightshape.net>
2024-01-27 16:47:57 +0100

    Fixed constant velocity increase by keeping lid open

--- Assets/Scripts/LidController.cs
+++ Assets/Scripts/LidController.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class LidController : MonoBehaviour
 {
     PlayerController _playerController;
-    Rigidbody _rb;
     HingeJoint _hingeJoint;
 
     PlayerInputs _playerInputs;
+    float _lastFrameJumpInput;
 
-    [SerializeField]
-    float _angularVelocityThreshhold = .2f;
+    float _lastOpeningTimestamp;
 
     [SerializeField]
     float _hingeJointMinLimit = -120;
 
+    [SerializeField]
+    float _timeToleranceLidOpenend = .1f;
+
     private void Awake()
     {
         _playerController = GetComponentInParent<PlayerController>();
-        _rb = GetComponent<Rigidbody>();
         _hingeJoint = GetComponent<HingeJoint>();
     }
 
@@ -30,15 +31,20 @@ public class LidController : MonoBehaviour
 
     private void Update()
     {
+        if (_playerInputs.Standard.Jump.ReadValue<float>() - _lastFrameJumpInput > 0)
+            _lastOpeningTimestamp = Time.timeSinceLevelLoad;
+
         //limit how far the lid can open according to input
         JointLimits limits = _hingeJoint.limits;
         limits.min = Mathf.Lerp(0, _hingeJointMinLimit, _playerInputs.Standard.Jump.ReadValue<float>());
         _hingeJoint.limits = limits;
+
+        _lastFrameJumpInput = _playerInputs.Standard.Jump.ReadValue<float>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (_rb.angularVelocity.z > _angularVelocityThreshhold)
+        if (Time.timeSinceLevelLoad - _lastOpeningTimestamp >= _timeToleranceLidOpenend)
             return;
 
         Vector3 averageCollisionPoint = Vector3.zero;
